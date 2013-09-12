4170671b9a60a9c186a0a0c498df811b2ccd330a
Juan Molina <juan.molina@club.fr>
2013-09-13 00:38:15 +0200

    Import old module from IAC site

--- code/GoogleSitemap.php
+++ code/GoogleSitemap.php
@@ -10,14 +10,14 @@
  * sitemap whenever the GoogleBot visits your website.
  * 
  * Enabling notification of Google after every publish (in your _config.php):
- *
- * <example>
+ * <example
  * GoogleSitemap::enable_google_notificaton();
  * </example>
  * 
  * @see http://www.google.com/support/webmasters/bin/answer.py?hl=en&answer=34609
  * 
- * @package googlesitemaps
+ * @package sapphire
+ * @subpackage misc
  */
 class GoogleSitemap extends Controller {
 	
@@ -27,164 +27,69 @@ class GoogleSitemap extends Controller {
 	protected static $enabled = true;
 	
 	/**
-	 * @var boolean
+	 * @var DataObjectSet
 	 */
-	protected static $google_notification_enabled = false;
+	protected $Pages;
 	
 	/**
 	 * @var boolean
 	 */
-	protected static $use_show_in_search = true;
-
-	/**
-	 * List of DataObjects to show in sitemap.xml
-	 *
-	 * @var array
-	 */
-	public static $google_sitemap_dataobjects = array();
-
-	/**
-	 * List of DataObjects change frequency
-	 *
-	 * @var array
-	 */
-	public static $google_sitemap_dataobjects_changefreq = array();
-        
-	/**
-	 * List of DataObjects priority
-	 * 
-	 * @var array 
-	 */
-	public static $google_sitemap_dataobjects_priority = array();
-
-	/**
-	 * Decorates the given DataObject with {@link GoogleSitemapDecorator}
-	 * and pushes the class name to the registered DataObjects.
-	 * Note that all registered DataObjects need the method AbsoluteLink().
-	 *
-	 * @param string $className  name of DataObject to register
-	 * @param string $changeFreq how often is this DataObject updated?
-	 *                           Possible values:
-	 *                           always, hourly, daily, weekly, monthly, yearly, never
-	 * @param string $priority   How important is this DataObject in comparison to other urls?
-	 *                           Possible values: 0.1, 0.2 ... , 0.9, 1.0
-	 *
-	 * @return void
-	 */
-	public static function register_dataobject($className, $changeFreq = 'monthly', $priority = '0.6') {
-		if (!self::is_registered($className)) {
-			Object::add_extension($className, 'GoogleSitemapDecorator');
-			
-			self::$google_sitemap_dataobjects[] = $className;
-			
-			if (!$changeFreq) {
-				self::$google_sitemap_dataobjects_changefreq[] = "monthly";
-			} else {
-				self::$google_sitemap_dataobjects_changefreq[] = $changeFreq;
-			}
-			
-			self::$google_sitemap_dataobjects_priority[] = $priority;
-		}
-	}
-
-	/**
-	 * Checks whether the given class name is already registered or not.
-	 *
-	 * @param string $className Name of DataObject to check
-	 * 
-	 * @return bool
-	 */
-	public static function is_registered($className) {
-		return in_array($className, self::$google_sitemap_dataobjects);
-	}
-
-	/**
-	 * Adds DataObjects to the existing DataObjectSet with pages from the
-	 * site tree
-	 * 
-	 * @return DataObjectSet 
-	 */
-	protected function addRegisteredDataObjects() {
-		$output = new DataObjectSet();
-		
-		foreach(self::$google_sitemap_dataobjects as $index => $className) {
-			$dataObjectSet = DataObject::get($className);
-			
-			if($dataObjectSet) {
-				foreach($dataObjectSet as $dataObject) {	
-					if($dataObject->canView()) {
-						$dataObject->ChangeFreq = self::$google_sitemap_dataobjects_changefreq[$index];
-						
-						if(!isset($dataObject->Priority)) {
-							$dataObject->Priority = self::$google_sitemap_dataobjects_priority[$index];
-						}
-						
-						$output->push($dataObject);
-					}
-				}
-			}
-		}
-		
-		return $output;
-	}
-
-	/**
-	 * Returns all the links to {@link SiteTree} pages and
-	 * {@link DataObject} urls on the page.
-	 *
-	 * @return DataObjectSet
-	 */
+	protected static $google_notification_enabled = false;
+	
 	public function Items() {
-		$filter = '';
+		$this->Pages = Versioned::get_by_stage('SiteTree', 'Live');
 
-		$bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`";
-		
-		if(self::$use_show_in_search) {
-			$filter = "{$bt}ShowInSearch{$bt} = 1";
-		}
-
-		$pages = Versioned::get_by_stage('SiteTree', 'Live', $filter);
-		
 		$newPages = new DataObjectSet();
 		
-		if($pages) {
-			foreach($pages as $page) {
-				// Only include pages from this host and pages which are not an 
-				// instance of ErrorPage. We prefix $_SERVER['HTTP_HOST'] with 
-				// 'http://' so that parse_url to help parse_url identify the 
-				// host name component; we could use another protocol (like ftp
-				// as the prefix and the code would work the same. 
-				$pageHttp = parse_url($page->AbsoluteLink(), PHP_URL_HOST);
-				$hostHttp = parse_url('http://' . $_SERVER['HTTP_HOST'], PHP_URL_HOST);
-				
-				if(($pageHttp == $hostHttp) && !($page instanceof ErrorPage)) {
-
-					// If the page has been set to 0 priority, we set a flag so 
-					// it won't be included
-					if($page->canView() && (!isset($page->Priority) || $page->Priority > 0)) { 
-						$page->setChangeFrequency();
-						$newPages->push($page);
+		foreach($this->Pages as $page) {
+			// Only include pages from this host and pages which are not an instance of ErrorPage 
+			// We prefix $_SERVER['HTTP_HOST'] with 'http://' so that parse_url to help parse_url identify the host name component; we could use another protocol (like 
+			// 'ftp://' as the prefix and the code would work the same. 
+			if(parse_url($page->AbsoluteLink(), PHP_URL_HOST) == parse_url('http://' . $_SERVER['HTTP_HOST'], PHP_URL_HOST) && !($page instanceof ErrorPage)) {
+				// If the page has been set to 0 priority, we set a flag so it won't be included
+				if($page->canView() && (!isset($page->Priority) || $page->Priority > 0)) { 
+					// The one field that isn't easy to deal with in the template is
+					// Change frequency, so we set that here.
+					$properties = $page->toMap();
+					$created = new SSDatetime();
+					$created->value = $properties['Created'];
+					$now = new SSDatetime();
+					$now->value = date('Y-m-d H:i:s');
+					$versions = $properties['Version'];
+					$timediff = $now->format('U') - $created->format('U');
+			
+					// Check how many revisions have been made over the lifetime of the
+					// Page for a rough estimate of it's changing frequency.
+			
+					$period = $timediff / ($versions + 1);
+			
+					if($period > 60*60*24*365) { // > 1 year
+						$page->ChangeFreq='yearly';
+					} elseif($period > 60*60*24*30) { // > ~1 month
+						$page->ChangeFreq='monthly';
+					} elseif($period > 60*60*24*7) { // > 1 week
+						$page->ChangeFreq='weekly';
+					} elseif($period > 60*60*24) { // > 1 day
+						$page->ChangeFreq='daily';
+					} elseif($period > 60*60) { // > 1 hour
+						$page->ChangeFreq='hourly';
+					} else { // < 1 hour
+						$page->ChangeFreq='always';
 					}
+				
+					$newPages->push($page);
 				}
 			}
-
 		}
-		
-		$newPages->merge($this->addRegisteredDataObjects());
-		
 		return $newPages;
 	}
 	
 	/**
 	 * Notifies Google about changes to your sitemap.
-	 *
 	 * Triggered automatically on every publish/unpublish of a page.
 	 * This behaviour is disabled by default, enable with:
-	 *
-	 * <code>
 	 * GoogleSitemap::enable_google_notificaton();
-	 * </code>
-	 *
+	 * 
 	 * If the site is in "dev-mode", no ping will be sent regardless wether
 	 * the Google notification is enabled.
 	 * 
@@ -197,24 +102,16 @@ class GoogleSitemap extends Controller {
 		if(!GoogleSitemap::$google_notification_enabled || Director::isDev())
 			return;
 			
-		$location = urlencode(Controller::join_links(
-			Director::absoluteBaseURL(), 
-			'sitemap.xml'
-		));
+		$location = urlencode(Director::absoluteBaseURL() . '/sitemap.xml');
 		
-		$response = HTTP::sendRequest(
-			"www.google.com", 
-			"/webmasters/sitemaps/ping",
-			sprintf("sitemap=%s", $location)
-		);
+		$response = HTTP::sendRequest("www.google.com", "/webmasters/sitemaps/ping",
+			"sitemap=" . $location);
 			
 		return $response;
 	}
 	
 	/**
 	 * Enable pings to google.com whenever sitemap changes.
-	 *
-	 * @return void
 	 */
 	public static function enable_google_notification() {
 		self::$google_notification_enabled = true;
@@ -222,43 +119,117 @@ class GoogleSitemap extends Controller {
 	
 	/**
 	 * Disables pings to google when the sitemap changes.
-	 *
-	 * @return void
 	 */
 	public static function disable_google_notification() {
 		self::$google_notification_enabled = false;
 	}
 	
-	/**
-	 * Default controller handler for the sitemap.xml file
-	 */
 	function index($url) {
 		if(self::$enabled) {
 			SSViewer::set_source_file_comments(false);
+			// We need to override the default content-type
+			ContentNegotiator::disable();
 			$this->getResponse()->addHeader('Content-Type', 'application/xml; charset="utf-8"');
 
 			// But we want to still render.
 			return array();
 		} else {
-			return new SS_HTTPResponse('Page not found', 404);
+			return new HTTPResponse('Not allowed', 405);
 		}
+
 	}
 	
-	/**
-	 * Enable the sitemap.xml file
-	 *
-	 * @return void
-	 */
 	public static function enable() {
 		self::$enabled = true;
 	}
 	
-	/**
-	 * Disable the sitemap.xml file
-	 *
-	 * @return void
-	 */
 	public static function disable() {
 		self::$enabled = false;
-	}     
+	}
 }
+
+/**
+ * @package sapphire
+ * @subpackage misc
+ */
+class GoogleSitemapDecorator extends SiteTreeDecorator {
+	
+	function extraStatics() {
+		return array(
+			'db' => array(
+				"Priority" => "Float",
+			)
+		);
+	}
+	
+	function updateCMSFields(&$fields) {
+		$pagePriorities = array(
+			'' => _t('SiteTree.PRIORITYAUTOSET','Auto-set based on page depth'),
+			'-1' => _t('SiteTree.PRIORITYNOTINDEXED', "Not indexed"), // We set this to -ve one because a blank value implies auto-generation of Priority
+			'1.0' => '1 - ' . _t('SiteTree.PRIORITYMOSTIMPORTANT', "Most important"),
+			'0.9' => '2',
+			'0.8' => '3',
+			'0.7' => '4',
+			'0.6' => '5',
+			'0.5' => '6',
+			'0.4' => '7',
+			'0.3' => '8',
+			'0.2' => '9',
+			'0.1' => '10 - ' . _t('SiteTree.PRIORITYLEASTIMPORTANT', "Least important")
+		);
+		
+		$tabset = $fields->findOrMakeTab('Root.Content');
+		$tabset->push(
+			$addTab = new Tab(
+				'GoogleSitemap',
+				_t('SiteTree.TABGOOGLESITEMAP', 'Google Sitemap'),
+				new LiteralField(
+					"GoogleSitemapIntro", 
+					"<p>" .
+					sprintf(
+						_t(
+							'SiteTree.METANOTEPRIORITY', 
+							"Manually specify a Google Sitemaps priority for this page (%s)"
+						),
+						'<a href="https://www.google.com/webmasters/tools/docs/en/protocol.html#prioritydef">?</a>'
+					) .
+					"</p>"
+				), 
+				new DropdownField("Priority", $this->owner->fieldLabel('Priority'), $pagePriorities)
+			)
+		);
+	}
+	
+	function updateFieldLabels(&$labels) {
+		parent::updateFieldLabels($labels);
+		
+		$labels['Priority'] = _t('SiteTree.METAPAGEPRIO', "Page Priority");
+	}
+	
+	function onAfterPublish() {
+		GoogleSiteMap::ping();
+	}
+	
+	function onAfterUnpublish() {
+		GoogleSiteMap::ping();
+	}
+	
+	/**
+	 * The default value of the priority field depends on the depth of the page in
+	 * the site tree, so it must be calculated dynamically.
+	 */
+	function getPriority() {		
+		if(!$this->owner->getField('Priority')) {
+			$parentStack = $this->owner->parentStack();
+			$numParents = is_array($parentStack) ? count($parentStack) - 1: 0;
+			return max(0.1, 1.0 - ($numParents / 10));
+		} elseif($this->owner->getField('Priority') == -1) {
+			return 0;
+		} else {
+			return $this->owner->getField('Priority');
+		}
+	}
+}
+
+Object::add_extension('SiteTree', 'GoogleSitemapDecorator');
+?>
